d53169e35cad717412dd8c989a29ccd1fceb5995
codeAmyth <dev.amyth27@gmail.com>
2025-10-09 11:06:51 +0530

    setting up relationship and unique data

--- src/main/java/com/ecommerce/ecommerce_app/model/Address.java
+++ src/main/java/com/ecommerce/ecommerce_app/model/Address.java
@@ -1,13 +1,57 @@
 package com.ecommerce.ecommerce_app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "address")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be at least 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters")
+    private String building;
+
+    @NotBlank
+    @Size(min = 3, message = "City name must be at least 3 characters")
+    private String city;
+
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 3, message = "Country name must be at least 3 characters")
+    private String country;
+
+
+    @NotBlank
+    @Size(min = 6, message = "Pincode must be at least 6 digits")
+    private String pincode;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(Long addressId, String pincode, String country, String state, String city, String building, String street) {
+        this.addressId = addressId;
+        this.pincode = pincode;
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.building = building;
+        this.street = street;
+    }
 }
--- src/main/java/com/ecommerce/ecommerce_app/model/Role.java
+++ src/main/java/com/ecommerce/ecommerce_app/model/Role.java
@@ -5,10 +5,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "role")
 public class Role {
     @Id
@@ -16,8 +18,14 @@ public class Role {
     @Column(name = "role_id")
     private Integer roleId;
 
-
+    @ToString.Exclude
     @Column(length = 20, name = "role_name")
     @Enumerated(EnumType.STRING) // by default enum sends int so converted into string
     private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+
 }
